27215c29f93958c40e68f4c33694c6b0e10a766b
Todd E Thomas <todd.dsm@gmail.com>
2017-02-28 12:22:11 -0600

    access/scret keys removed; given aws cli is properly setup, there is no need to define them in Terraform files

--- variables.tf
+++ variables.tf
@@ -1,16 +1,9 @@
 variable "aws_account_id" {
   description = "AWS account id"
-}
-
-variable "aws_access_key" {
-  description = "AWS access key"
-}
-
-variable "aws_secret_key" {
-  description = "AWS secret key"
+  default = "405322537961"
 }
 
 variable "aws_region" {
   description = "AWS region"
-  default     = "eu-west-1"
+  default     = "us-west-2"
 }
